8ef05a03ba139adfd7ba3ba95d9a7c24e0284ccd
Fauz Ahmed <fauz.ahmed369@gmail.com>
2019-10-04 13:38:35 -0500

    added database call

--- GreatBae.js
+++ GreatBae.js
@@ -6,9 +6,11 @@ var connection = mysql.createConnection({
     host:'localhost',
     port: 3306,user: 'root',
     password: process.env.DB_PASSWORD,
-    database:""
+    database:"greatbay_db"
 });
 
-function run () {
-    inquirer
-}
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+});
+
