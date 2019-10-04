a9d9a607343b2ca53e0d70a5664264b1d0e2e9ed
Daniel Masters <daniel.p.masters@gmail.com>
2019-10-04 13:49:20 -0500

    initial inquirer ask

--- GreatBae.js
+++ GreatBae.js
@@ -1,1 +1,40 @@
 require("dotenv").config();
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+
+function run() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Post or Bid?",
+                name: "userDefiner"
+            }
+        ]).then(answer => {
+            function switchIt() {
+                switch (answer.userDefiner) {
+                    case "post":
+                        postItem();
+                        break;
+                    case "bid":
+                        bid();
+                        break;
+                    default: console.log("Please choose a valid command");
+                        run();
+                }
+            };
+        })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
